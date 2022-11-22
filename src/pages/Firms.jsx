7ee75e20961accc55c8df3a2e88f2b29dbfae90b
hasan <hasanakoz1986@gmail.com>
2022-11-22 21:16:50 +0300

    getfirms function

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,4 +1,31 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+
 const Firms = () => {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://10001.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    const url = "firms";
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      console.log(data);
+      dispatch(getSuccess({ data, url }));
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
   return <div>Firms</div>;
 };
 
