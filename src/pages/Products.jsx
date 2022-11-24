e819a239d695f30964a3e43732c40386b27c0f3d
hasan <hasanakoz1986@gmail.com>
2022-11-24 22:31:58 +0300

    product sort hooks done

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -15,29 +15,45 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { btnHoverStyle, flexCenter } from "../styles/globalStyles";
 import UpgradeIcon from "@mui/icons-material/Upgrade";
 import VerticalAlignBottomIcon from "@mui/icons-material/VerticalAlignBottom";
+import useSortColumn from "../hooks/useSortColumn";
+import { MultiSelectBox, MultiSelectBoxItem } from "@tremor/react";
 // import { useDispatch, useSelector } from "react-redux";
 // import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Products = () => {
-  const { getBrands, getCategories, getProducts } = useStockCalls();
+  const { getBrands, getCategories, getProducts, brands } = useStockCalls();
   const { products } = useSelector((state) => state.stock);
   const [open, setOpen] = useState(false);
   const [info, setInfo] = useState({});
+  const [selectedBrands, setSelectedBrands] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
 
-  const [toggle, setToggle] = useState({
-    brand: false,
-    name: false,
+  const columnObj = {
+    brand: 1,
+    name: 1,
     stock: 1,
-  });
+  };
+
+  const { sortedData, handleSort, columns } = useSortColumn(
+    products,
+    columnObj
+  );
+
   useEffect(() => {
     getBrands();
     getCategories();
     getProducts();
   }, []);
 
-  const handleSortNumber = (arg) => {
-    setToggle({ ...toggle, [arg]: toggle[arg] * -1 });
-  };
+  const isBrandSelected = (item) =>
+    selectedBrands.includes(item.brand) || selectedBrands.length === 0;
+
+  const isProductSelected = (item) =>
+    selectedProducts.includes(item.name) || selectedProducts.length === 0;
+
+  const filteredProducts = products
+    ?.filter((item) => selectedBrands.includes(item.brand))
+    .map((item) => item.name);
 
   return (
     <Box>
@@ -48,7 +64,31 @@ const Products = () => {
         Add New Product
       </Button>
 
-      {products?.length >= 0 && (
+      <Box>
+        <MultiSelectBox
+          handleSelect={(item) => setSelectedBrands(item)}
+          placeholder="Select Brand"
+        >
+          {brands?.map((item) => (
+            <MultiSelectBoxItem
+              key={item.name}
+              value={item.name}
+              text={item.name}
+            />
+          ))}
+        </MultiSelectBox>
+
+        <MultiSelectBox
+          handleSelect={(item) => setSelectedProducts(item)}
+          placeholder="Select Brand"
+        >
+          {filteredProducts?.map((item) => (
+            <MultiSelectBoxItem key={item} value={item} text={item} />
+          ))}
+        </MultiSelectBox>
+      </Box>
+
+      {sortedData?.length > 0 && (
         <TableContainer component={Paper} sx={{ mt: 3 }} elevation={10}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -56,50 +96,58 @@ const Products = () => {
                 <TableCell align="center">#</TableCell>
                 <TableCell align="center">Category</TableCell>
                 <TableCell align="center" sx={btnHoverStyle}>
-                  <Box sx={flexCenter}>
+                  <Box
+                    sx={flexCenter}
+                    onClick={() => handleSort("brand", "text")}
+                  >
                     <div>Brand</div>
-                    {true && <UpgradeIcon />}
-                    {false && <VerticalAlignBottomIcon />}
-                  </Box>
-                </TableCell>
-                <TableCell align="center" sx={btnHoverStyle}>
-                  <Box sx={flexCenter}>
-                    <div>Name</div>
-                    {true && <UpgradeIcon />}
-                    {false && <VerticalAlignBottomIcon />}
+                    {columns.brand === 1 && <UpgradeIcon />}
+                    {columns.brand !== 1 && <VerticalAlignBottomIcon />}
                   </Box>
                 </TableCell>
                 <TableCell align="center" sx={btnHoverStyle}>
                   <Box
                     sx={flexCenter}
-                    onClick={() => handleSortNumber("stock")}
+                    onClick={() => handleSort("name", "text")}
+                  >
+                    <div>Name</div>
+                    {columns.name === 1 && <UpgradeIcon />}
+                    {columns.name !== 1 && <VerticalAlignBottomIcon />}
+                  </Box>
+                </TableCell>
+                <TableCell align="center" sx={btnHoverStyle}>
+                  <Box
+                    sx={flexCenter}
+                    onClick={() => handleSort("stock", "number")}
                   >
                     <div>Stock</div>
-                    {toggle.stock === 1 && <UpgradeIcon />}
-                    {toggle.stock !== 1 && <VerticalAlignBottomIcon />}
+                    {columns.stock === 1 && <UpgradeIcon />}
+                    {columns.stock !== 1 && <VerticalAlignBottomIcon />}
                   </Box>
                 </TableCell>
                 <TableCell align="center">Operation</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {products.map((product, index) => (
-                <TableRow
-                  key={product.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="center" component="th" scope="row">
-                    {index + 1}
-                  </TableCell>
-                  <TableCell align="center">{product.category}</TableCell>
-                  <TableCell align="center">{product.brand}</TableCell>
-                  <TableCell align="center">{product.name}</TableCell>
-                  <TableCell align="center">{product.stock}</TableCell>
-                  <TableCell align="center">
-                    <DeleteIcon sx={btnHoverStyle} />
-                  </TableCell>
-                </TableRow>
-              ))}
+              {sortedData
+                .filter((item) => isBrandSelected(item))
+                .map((product, index) => (
+                  <TableRow
+                    key={product.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="center" component="th" scope="row">
+                      {index + 1}
+                    </TableCell>
+                    <TableCell align="center">{product.category}</TableCell>
+                    <TableCell align="center">{product.brand}</TableCell>
+                    <TableCell align="center">{product.name}</TableCell>
+                    <TableCell align="center">{product.stock}</TableCell>
+                    <TableCell align="center">
+                      <DeleteIcon sx={btnHoverStyle} />
+                    </TableCell>
+                  </TableRow>
+                ))}
             </TableBody>
           </Table>
         </TableContainer>
